6d46ada7ba3715cc8344128e8c685a014c9a46c1
vairakkani <vairakkani@gmail.com>
2022-03-18 18:22:55 +0530

    rendering a html page from template folder

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@app.route('/')
+def index():
+   return render_template('index.html')
 
 
 if __name__ == "__main__":
